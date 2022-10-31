2d6c683153757008055e3373c8a135f764566aa3
Vladimir Em <vem@ucsd.edu>
2022-10-30 19:18:41 -0700

    Speech Test 1

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,35 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let selectVoice = document.getElementById("voice-select");
+  let currChoice;
+  voices = synth.getVoices();
+
+  const name = '';
+
+  selectVoice.addEventListener('change', (event) => {
+
+    name = event.target.value;
+        
+
+  });
+
+  
+
+  const buttonPressToTalk = document.querySelector('button');
+  const message = document.getElementById('text-to-speak"')
+
+  buttonPressToTalk.addEventListener('click', (event) => {
+    
+    const utterThis = new SpeechSynthesisUtterance(message.value);
+    for (let i = 0; i < voices.length ; i++) {
+      if (voices[i].name === name) {
+        utterThis.voice = voices[i];
+      }
+    }
+  
+    synth.speak(utterThis);
+    
+  });
+
 }
