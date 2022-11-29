2e85bc4674587406fd687255ae8f03d3e136b58f
Vladimir Em <vem@ucsd.edu>
2022-11-28 17:38:51 -0800

    Fix Reuploaded

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,6 +1,26 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+function populateVoiceList() {
+  if (typeof speechSynthesis === 'undefined') {
+    return;
+  }
+
+  const voices = speechSynthesis.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += ' — DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    document.getElementById("voiceSelect").appendChild(option);
+  }
+}
 
 function init() {
   const synth = window.speechSynthesis;
@@ -46,23 +66,17 @@ function init() {
  
   const buttonPressToTalk = document.querySelector('button');
   const message = document.getElementById('text-to-speak')
+  populateVoiceList();
   buttonPressToTalk.addEventListener('click', (event) => {
     const utterThis = new SpeechSynthesisUtterance(message.value);
-    for (let i = 0; i < voices.length ; i++) {
-      if (voices[i].name === voice) {
-        utterThis.voice = voices[i];
-      }
-    }
-    
-
     utterThis.pitch = 1;
     utterThis.rate = 1;
     synth.speak(utterThis);
     document.querySelector("img").src=`assets/images/smiling-open.png`
     utterThis.addEventListener("end", (event)=>{
       document.querySelector("img").src=`assets/images/smiling.png`;
+  });
   
-    });
   });
   
 
